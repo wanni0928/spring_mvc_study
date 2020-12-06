493fe3c351b8ca2e0905cf7bb045fa0ad2f36926
wanni0928 <chlgudwnd123@gmail.com>
2020-12-06 22:46:23 +0900

    Update SampleController2.java

--- sping-mvc-practice/src/main/java/com/wannistudio/spingmvcpractice/SampleController2.java
+++ sping-mvc-practice/src/main/java/com/wannistudio/spingmvcpractice/SampleController2.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+//@Controller
 @RequestMapping(value = "/cat")
 public class SampleController2 {
     @GetMapping(value = "/**")
