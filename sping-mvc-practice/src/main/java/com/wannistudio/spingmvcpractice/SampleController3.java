424db2088ae67dd4a0dea7e149720679b30300be
wanni0928 <chlgudwnd123@gmail.com>
2020-12-06 22:46:27 +0900

    Update SampleController3.java

--- sping-mvc-practice/src/main/java/com/wannistudio/spingmvcpractice/SampleController3.java
+++ sping-mvc-practice/src/main/java/com/wannistudio/spingmvcpractice/SampleController3.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+//@Controller
 public class SampleController3 {
     @GetMapping(value = "/world/{id}"
 //            , headers = HttpHeaders.FROM
